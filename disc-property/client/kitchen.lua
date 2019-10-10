3ebab7af4560d8428d4409fe0b41c81b88cef2d1
TraficOfPeras <56407925+TraficOfPeras@users.noreply.github.com>
2019-10-10 18:47:54 +0100

    Add lock function
    Just adds the ability to enter home if the door is unlocked

--- disc-property/client/kitchen.lua
+++ disc-property/client/kitchen.lua
@@ -1,9 +1,19 @@
 function OpenKitchen(property)
 
     local options = {
-        { label = 'Make Food', action = MakeFood }
+        { label = 'Make Food', action = MakeFood },
     }
-
+    
+    if not IsUnlocked(property) then
+        table.insert(options, { label = 'Lock the house', action = function()
+            Trancamento(property, 1)
+        end })
+    elseif IsUnlocked(property) then
+        table.insert(options, { label = 'Unlock the house', action = function()
+            Trancamento(property, 2)
+        end })
+    end
+    
     if IsPlayerOwnerOf(property) then
         table.insert(options, { label = 'Manage Property', action = function()
             ShowManageProperty(property)
@@ -21,4 +31,17 @@ end
 
 function MakeFood()
     TriggerServerEvent('disc-base:givePlayerItem', Config.FoodItem, 1)
+end
+
+function Trancamento(property, info)
+    ESX.UI.Menu.CloseAll()
+	if info == 1 then
+		exports['mythic_notify']:DoHudText('error', 'The house has been locked!')
+		TriggerServerEvent('disc-property:trancarProperty', property, info)
+		TriggerEvent('disc-property:forceUpdatePropertyData')
+	else
+		exports['mythic_notify']:DoHudText('success', 'The house has been unlocked!')
+		TriggerServerEvent('disc-property:trancarProperty', property, info)
+		TriggerEvent('disc-property:forceUpdatePropertyData')
+	end
 end
